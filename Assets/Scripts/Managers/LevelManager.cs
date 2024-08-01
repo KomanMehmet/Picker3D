8c2f6dc95f60360e96e4172b145b418ff4462a3e
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2024-08-01 17:12:23 +0300

    commit_levelManager

---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,95 @@
+﻿using Commands.Level;
+using Data.UnityObjects;
+using Data.ValueObjects;
+using Signals;
+using UnityEngine;
+
+namespace Managers
+{
+    public class LevelManager : MonoBehaviour
+    {
+        //SerializeField Variables
+        [SerializeField] private Transform levelHolder;
+        [SerializeField] private byte totalLevelCount;
+        
+        //Private Variables
+        private short _currentLevel;
+        private LevelData _levelData;
+
+        private OnLevelLoaderCommand _levelLoaderCommand;
+        private OnLevelDestroyerCommand _levelDestroyerCommand;
+
+        private void Awake()
+        {
+            _levelData = GetlevelData();
+            _currentLevel = GetActiveLevel();
+
+            Init();
+        }
+
+        private void Start()
+        {
+            CoreGameSignals.Instance.onLevelInitialize?.Invoke((byte)(_currentLevel % totalLevelCount));
+            //UISignals
+        }
+
+        private void Init()
+        {
+            _levelLoaderCommand = new OnLevelLoaderCommand(levelHolder);
+            _levelDestroyerCommand = new OnLevelDestroyerCommand(levelHolder);
+        }
+        
+        private LevelData GetlevelData()
+        {
+            return Resources.Load<CD_Level>("Data/CD_Level").Levels[_currentLevel];
+        }
+        
+        private short GetActiveLevel()
+        {
+            //_levelLoaderCommand.Execute();
+            return _currentLevel;
+        }
+
+        private void OnEnable() => SubscribeEvents();
+        
+        private void SubscribeEvents()
+        {
+            CoreGameSignals.Instance.onLevelInitialize += _levelLoaderCommand.Execute;
+            CoreGameSignals.Instance.onClearActiveLevel += _levelDestroyerCommand.Execute;
+            CoreGameSignals.Instance.onGetLevelValue += OnGetLevelValue;
+            CoreGameSignals.Instance.onNextLEvel += OnNextLevel;
+            CoreGameSignals.Instance.OnRestartLevel += OnRestartLevel;
+        }
+        
+        private void OnNextLevel()
+        {
+            _currentLevel++;
+            CoreGameSignals.Instance.onClearActiveLevel?.Invoke();
+            CoreGameSignals.Instance.onReset?.Invoke();
+            CoreGameSignals.Instance.onLevelInitialize?.Invoke((byte)(_currentLevel % totalLevelCount));
+        }
+
+        private void OnRestartLevel()
+        {
+            CoreGameSignals.Instance.onClearActiveLevel?.Invoke();
+            CoreGameSignals.Instance.onReset?.Invoke();
+            CoreGameSignals.Instance.onLevelInitialize?.Invoke((byte)(_currentLevel % totalLevelCount)); 
+        }
+
+        private byte OnGetLevelValue()
+        {
+            return (byte)_currentLevel;
+        }
+        
+        private void UnSubscribeEvents()
+        {
+            CoreGameSignals.Instance.onLevelInitialize -= _levelLoaderCommand.Execute;
+            CoreGameSignals.Instance.onClearActiveLevel -= _levelDestroyerCommand.Execute;
+            CoreGameSignals.Instance.onGetLevelValue -= OnGetLevelValue;
+            CoreGameSignals.Instance.onNextLEvel -= OnNextLevel;
+            CoreGameSignals.Instance.OnRestartLevel -= OnRestartLevel;
+        }
+        
+        private void OnDisable() => UnSubscribeEvents();
+    }
+}
---
+++ Assets/Scripts/Signals/CoreGameSignals.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Signals
+{
+    public class CoreGameSignals : MonoBehaviour
+    {
+        public static CoreGameSignals Instance;
+
+        public UnityAction<byte> onLevelInitialize = delegate {  };
+        public UnityAction onClearActiveLevel = delegate {  };
+        public UnityAction onNextLEvel = delegate {  };
+        public UnityAction OnRestartLevel = delegate {  };
+        public UnityAction onReset = delegate {  };
+        public Func<byte> onGetLevelValue = delegate { return 0; };
+        
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+        }
+    }
+}
