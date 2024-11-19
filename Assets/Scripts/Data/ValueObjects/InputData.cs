0fc7a19195ea258f8b5ef14366c0ee399e8d219d
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2024-11-20 00:35:23 +0300

    Input System Completed

---
+++ Assets/Scripts/Data/ValueObjects/InputData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System;
+
+namespace Data.ValueObjects
+{
+    [Serializable]
+    public struct InputData
+    {
+        public float HorizontalInputSpeed;
+        public Vector2 ClampValues;
+        public float ClampSpeed;
+    }
+}
