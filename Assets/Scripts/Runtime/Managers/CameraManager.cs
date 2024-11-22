5632c73a78a6c91a5cfd3715e0aad83dbcaa8cd5
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2024-11-22 16:45:21 +0300

    Camera System Finished

---
+++ Assets/Scripts/Runtime/Managers/CameraManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Runtime.Signals;
+using Unity.Cinemachine;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Runtime.Managers
+{
+    public class CameraManager : MonoBehaviour
+    {
+        [SerializeField] private CinemachineCamera cinemachineCamera;
+
+        private float3 _firstPosition;
+
+        private void Start()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            _firstPosition = transform.position;
+        }
+
+        private void OnEnable()
+        {
+            SubscribeEvents();
+        }
+
+        private void SubscribeEvents()
+        {
+            CameraSignals.Instance.onSetCameraTarget += OnSetCameraTarget;
+            CoreGameSignals.Instance.onReset += OnReset;
+        }
+
+        private void OnSetCameraTarget()
+        {
+            //var player = FindObjectOfType<PlayerManager>().transform;
+            //cinemachineCamera.Follow = player;
+            //cinemachineCamera.LookAt = player;
+        }
+
+        private void OnReset()
+        {
+            transform.position = _firstPosition;
+        }
+        
+        private void UnSubscribeEvents()
+        {
+            CameraSignals.Instance.onSetCameraTarget -= OnSetCameraTarget;
+            CoreGameSignals.Instance.onReset -= OnReset;
+        }
+
+        private void OnDisable()
+        {
+            UnSubscribeEvents();
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/Signals/CameraSignals.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Runtime.Signals
+{
+    public class CameraSignals : MonoBehaviour
+    {
+        public static CameraSignals Instance;
+
+        private void Awake()
+        {
+            if(Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+        }
+        
+        public UnityAction onSetCameraTarget = delegate {  };
+    }
+}
